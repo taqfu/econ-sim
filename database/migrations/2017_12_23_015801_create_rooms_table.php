df49093a3928cd770c7c2ec6450fa94f589b35f8
taqfu <taqfu0@gmail.com>
2017-12-23 02:17:59 +0000

    Needed to also add RoomTypes

--- database/migrations/2017_12_23_015801_create_rooms_table.php
+++ database/migrations/2017_12_23_015801_create_rooms_table.php
@@ -16,6 +16,14 @@ class CreateRoomsTable extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->char('name', 255);
+            $table->unsignedInteger('building_id');
+            $table->unsignedInteger('current_storage');
+            $table->unsignedInteger('max_storage');
+            $table->boolean('public')->default('false');
+            $table->boolean('enclosed')->default('true');
+            $table->boolean('sleep')->default('false');
+
         });
     }
 
